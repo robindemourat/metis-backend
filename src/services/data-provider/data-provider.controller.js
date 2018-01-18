06ddbf630214251464d19b146e57ae01a7835984
Robin de Mourat <robin.demourat@gmail.com>
2018-01-19 00:49:09 +0100

    implement asset public provider

--- src/services/data-provider/data-provider.controller.js
+++ src/services/data-provider/data-provider.controller.js
@@ -1,11 +1,24 @@
 
 import {dal as montageDal} from '../../components/montages';
+import {dal as assetDal} from '../../components/assets';
 import {fetchMontageDependencies} from '../../utils/data';
 
 
 
 export const getAssetAttachmentUrl = (req, res) => {
-  return res.send('');
+  const id = req.params.id;
+  return assetDal.getAsset({id})
+          .then(asset => {
+            return assetDal.getAssetAttachment({
+              id,
+              filename: asset.filename
+            })
+          })
+          .then(({data, mimetype}) => {
+            res.writeHead(200, {'Content-Type': mimetype});
+            res.end(data, 'binary');
+          })
+          .catch(e => res.status(500).send(e));
 };
 
 
